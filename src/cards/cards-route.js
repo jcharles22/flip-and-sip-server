397719bb9d325b530db8f3e5e0f434f6b8c66809
Jared Charles <jaredcharles22@gmail.com>
2019-07-27 11:31:31 -0400

    added conosle.logs

--- src/cards/cards-route.js
+++ src/cards/cards-route.js
@@ -10,7 +10,6 @@ const bodyParser = express.json();
 cardRouter
   .route('/')
   .get( bodyParser, (req, res, next) => {
-    console.log('a')
     const knexInstance = req.app.get('db')
     let userId = req.get("userName")
       CardService.getAllCards(knexInstance, userId)
@@ -41,34 +40,37 @@ cardRouter
   })
     .post(requireAuth, jsonParser, (req, res, next) => {
       let { card_title, card_desc, deck } = req.body
-      const newCard = {  card_title, card_desc,}
+      console.log(req.body)
+      const newCard = {  card_title, card_desc}
       newCard.author = req.user.id
+      console.log('req.user ' +req.user.id)
       let card=[];
         CardService.insertCard(
           req.app.get('db'),
           newCard
        ) .then(cardId => {
-         return CardService.getAllUsersByIds(req.app.get('db'))
-          .then(users=> {
-              let card=[]
-              let counter = 0
-              deck.forEach((deck, count) => { 
-                
-                let deckId =parseInt(deck)
-                users.forEach((user, index)=> {
-                  return(
-                  card[counter]={users:user.id, card_id: cardId.card_id, deck_id:deckId}
-                  ,counter++
-                  )
+         console.log('cardId = '+ cardId )
+          return CardService.getAllUsersByIds(req.app.get('db'))
+            .then(users=> {
+                let card=[]
+                let counter = 0
+                deck.forEach((deck, count) => { 
+                  console.log('countet: ' + counter)
+                  let deckId =parseInt(deck)
+                  users.forEach((user, index)=> {
+                    return(
+                      card[counter]={users:user.id, card_id: cardId.card_id, deck_id:deckId},
+                      counter++
+                    )
+                  })
                 })
-              })
 
-              return CardService.updateDeck_card(req.app.get('db'), card)
-                .then(response=> res.status(204).send())
-          }).catch(next)
+                return CardService.updateDeck_card(req.app.get('db'), card)
+                  .then(response=> res.status(204).send())
+            }).catch(error => console.error(error))
 
-       }) .then(response =>{return null})
-       .catch(next)
+        }) .then(response =>{return null})
+            .catch(error => console.log(error))
   })
     
 
